a6b375454ede14353022ceaa722117ef9b1439bb
Steve Lane <lane.s@unimelb.edu.au>
2016-12-01 09:16:46 +1100

    Add documentation

--- R/setupData.R
+++ R/setupData.R
@@ -1,3 +1,10 @@
+#' Setup a new database
+#'
+#' \code{setupData} Sets up a new database to store the expert elicitations in.
+#'
+#' @param dbName The name of the database to store the elicitations in.
+#' @return No object is returned, the database is created.
+
 setupData <- function(dbName){
     myDB <- DBI::dbConnect(RSQLite::SQLite(),
                            paste0("../data/", dbName, ".sqlite"))
