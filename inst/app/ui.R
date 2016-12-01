5f7722ef4c99e22c94c77af19e89fdd66d1f230c
Steve Lane <lane.s@unimelb.edu.au>
2016-12-02 09:01:47 +1100

    Simple form added
    Use shinyjs and DT packages.

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,9 +1,24 @@
 library(shiny)
 
+## Name the fields required here - there will have to be a better way though. I
+## think the database should have an experts table and an elicitations table
+## fields should probably be read from the created database, and a consistent
+## identifier field used.
+
 shinyUI(fluidPage(
-    textInput("name", "Name", ""),
-    checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
-    sliderInput("r_num_years", "Number of years using R",
-                0, 25, 2, ticks = FALSE),
-    actionButton("submit", "Submit")
+    sidebarLayout(
+        sidebarPanel(
+            textInput("round", "Round", ""),
+            textInput("expert", "Expert", ""),
+            numericInput("best_guess", "Best guess", ""),
+            numericInput("lower", "Lower limit", ""),
+            numericInput("upper", "Upper limit", ""),
+            numericInput("confidence", "Confidence", ""),
+            actionButton("submit", "Submit")
+        ),
+        mainPanel(
+            DT::dataTableOutput("responses")
+        )
+    ),
+    shinyjs::useShinyjs()
 ))
