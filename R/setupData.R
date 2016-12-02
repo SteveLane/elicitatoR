bfb9f69ef193ce0357d009891bb29a640ee5f60f
Steve Lane <lane.s@unimelb.edu.au>
2016-12-02 16:55:18 +1100

    If data directory doesn't exist, create it
    Disconnect from DB after creation.

--- R/setupData.R
+++ R/setupData.R
@@ -3,11 +3,16 @@
 #' \code{setupData} Sets up a new database to store the expert elicitations in.
 #'
 #' @param dbName The name of the database to store the elicitations in.
-#' @return No object is returned, the database is created.
+#' @return No object is returned, the database is created. Note: the database is
+#'     created in the data directory (which is created if it doesn't exist).
 
 setupData <- function(dbName){
+    if(!(dir.exists("data"))){
+        dir.create("data")
+    }
     myDB <- DBI::dbConnect(RSQLite::SQLite(),
-                           paste0("../data/", dbName, ".sqlite"))
+                           paste0("data/", dbName, ".sqlite"))
     DBI::dbSendQuery(myDB,
-                     "CREATE TABLE elicitations (round INTEGER, expert TEXT, lower DOUBLE, best_guess DOUBLE, upper DOUBLE, confidence DOUBLE)")
+                     "CREATE TABLE elicitations (round INTEGER, expert TEXT, best_guess DOUBLE, lower DOUBLE, upper DOUBLE, confidence DOUBLE)")
+    DBI::dbDisconnect(myDB)
 }
