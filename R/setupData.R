fe07541e3c0a9819b8c2d55ece8021ca9142a84d
Steve Lane <lane.s@unimelb.edu.au>
2016-12-21 08:36:32 +1100

    Change to dbExecute

--- R/setupData.R
+++ R/setupData.R
@@ -10,9 +10,11 @@ setupData <- function(dbName){
     if(!(dir.exists("data"))){
         dir.create("data")
     }
-    myDB <- DBI::dbConnect(RSQLite::SQLite(),
-                           paste0("data/", dbName, ".sqlite"))
-    DBI::dbGetQuery(myDB,
-                    "CREATE TABLE elicitations (round INTEGER, expert TEXT, best_guess DOUBLE, lower DOUBLE, upper DOUBLE, confidence DOUBLE)")
-    DBI::dbDisconnect(myDB)
+    if(!file.exists(paste0("data/", dbName, ".sqlite"))){
+        myDB <- DBI::dbConnect(RSQLite::SQLite(),
+                               paste0("data/", dbName, ".sqlite"))
+        DBI::dbExecute(myDB,
+                        "CREATE TABLE elicitations (round INTEGER, expert TEXT, best_guess DOUBLE, lower DOUBLE, upper DOUBLE, confidence DOUBLE, alpha DOUBLE, beta DOUBLE)")
+        DBI::dbDisconnect(myDB)
+    }
 }
