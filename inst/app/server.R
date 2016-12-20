81d7f98b60a723dcdd8d45f30328b01378e687e6
Steve Lane <lane.s@unimelb.edu.au>
2016-12-21 08:36:16 +1100

    Compile form data in a better way

--- inst/app/server.R
+++ inst/app/server.R
@@ -9,15 +9,16 @@ shinyServer(function(input, output, session) {
     ## Enable input fields when database name is set
     observe({
         isDataSet <- !is.null(input[["dbName"]]) && input[["dbName"]] != ""
-        sapply(fields, function(x) shinyjs::toggleState(x, isDataSet))
         shinyjs::enable("submitDBName")
     })
     
     ## Setup database on submit
     observeEvent(input$submitDBName, {
-        saveData(formData())
+        ## saveData(formData())
+        setupData(input[["dbName"]])
         shinyjs::disable("dbName")
         shinyjs::disable("submitDBName")
+        ## sapply(fields, function(x) shinyjs::enable(x))
     })
     
     ## Enable the Submit button when all fields are filled out
@@ -31,20 +32,36 @@ shinyServer(function(input, output, session) {
     
     ## Whenever a field is filled, aggregate all form data
     formData <- reactive({
-        data <- sapply(fields, function(x){input[[x]]})
-        data
+        data <- lapply(fields, function(x){
+            dat <- data.frame(input[[x]])
+            names(dat) <- x
+            dat
+        })
+        as.data.frame(data)
     })
     
     ## When the Submit button is clicked, save the form data
     ## Not sure I need the save function above.
     observeEvent(input$submit, {
-        saveData(formData())
+        fitPars <- fitOpt(formData())
+        saveData(cbind.data.frame(formData(), fitPars), input[["dbName"]])
+        shinyjs::reset("form")
     })
+
+    ## Load data on submit for printing
+    allData <- reactive({
+        input[["submit"]]
+        loadData(input[["dbName"]])
+    })  
     
     ## Show the previous responses
     ## (update with current response when Submit is clicked)
     output$responses <- DT::renderDataTable({
-        input$submit
-        loadData()
+        DT::datatable(
+                allData(),
+                rownames = FALSE,
+                options = list(searching = FALSE, lengthChange = FALSE,
+                               scrollX = TRUE)
+            )
     })
 })
