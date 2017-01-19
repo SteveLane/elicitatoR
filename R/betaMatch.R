9d30a2aacbb16c66c059e7c3b160a283e048f901
Steve Lane <lane.s@unimelb.edu.au>
2017-01-19 14:23:18 +1100

    Revert "Export functions for fitting optimal beta"
    This reverts commit 2c8ea1dcb759af91b561ec1e5958480757cbb02e.

--- R/betaMatch.R
+++ R/betaMatch.R
@@ -6,9 +6,6 @@
 #' @param par Target of optimisation; not set by user.
 #' @param elicited Data frame of elicited values.
 #' @return No object is returned.
-#'
-#' @export elicitatoR
-#' @export fitOpt
 
 betaMatch <- function(par, elicited){
     b <- exp(par)
